37988eb58ebd890a9fb579900fb0c44f33329173
Bogdan Polygalov <77894393+miamib34ch@users.noreply.github.com>
2021-10-04 18:23:36 +0300

    Update laba 3.pas

--- laba 3.pas
+++ laba 3.pas
@@ -1,21 +1,21 @@
 {$asmmode intel}
 {$output format asw}
-var a, c: longint;
+var a, c: longint; //longint - 4 байтные числа
 begin
 a:= 999;
 asm
 lea esi, a
 lea edi, c
-mov bl, 10
-mov edx,4
+mov bl, 10 //умножение на 1 байтовое число
+mov edx, 4
 
 @1:
-mov al,[esi]
-mul bl
-add al,ch
-mov ch,ah
-mov [edi], al
-inc esi
+mov al,[esi]      //берём 0 байт, копируем в al
+mul bl            //умножение
+add al,ch         //добавляем к al ch(в нём ah от прошлого цикла, для первого 0)
+mov ch,ah 
+mov [edi], al     //записываем полученный байт в 0 байт числа вывода
+inc esi           //esi:=esi+1
 inc edi
 sub edx,1
 jnz @1
